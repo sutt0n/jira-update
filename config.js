3e9ab2875ad7db1394a77e0dc44b72210344a985
Joseph Sutton <sutt0n@users.noreply.github.com>
2017-12-10 19:59:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
 	protocol: "https",						// protocol
-	host: process.env.JIRA_HOST,			// hostname
-	user: process.env.JIRA_USERNAME,		// jira username
-	password: process.env.JIRA_PASSWORD,	// jira password
+	host: process.env.JIRA_HOST,					// hostname
+	user: process.env.JIRA_USERNAME,				// jira username
+	password: process.env.JIRA_PASSWORD,				// jira password
 	version: "latest",						// jira api version
 	runEvery: 5, 							// seconds
-	notificationTimeout: 5,					// seconds
+	notificationTimeout: 5,						// seconds
 	os: "windows"							// windows, other
 };
 
